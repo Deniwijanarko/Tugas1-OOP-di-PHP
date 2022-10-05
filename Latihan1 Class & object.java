d9cc80c9ce440e417ebdcc4adc5a9cbd30050bfb
Deni wijanarko <105209938+Deniwijanarko@users.noreply.github.com>
2022-10-05 15:55:30 +0700

    Add files via upload

---
+++ Latihan1 Class & object.java
@@ -0,0 +1,19 @@
+<?php
+//class mobil
+class mobil{
+        public $nama;
+        public $merk:
+
+        function getinfo() {
+            echo "nama mobil: ".$this->nama."<br/>";
+            echo "merk: ".$this->merk."<br/>"
+        }
+}
+
+//bagian main
+$ferari-new mobil();
+$ferari->nama-"488";
+$ferari->merk-"pistaspeeder";
+
+$ferari->getinfo();
+?>
